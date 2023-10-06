fad4ccf82dbed307cd4d157ad15a1fb1dfd68df1
o.hrachov <ol.grachov@gmail.com>
2023-10-06 13:40:38 +0300

    init commit

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+require('dotenv').config();
+import express from "express";
+import cors from "cors";
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+
+app.listen(PORT, () => console.log(`server started on port => ${PORT}`));
