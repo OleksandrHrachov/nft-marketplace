6549360bfc5cd25c5f1a673d3bf294e110902898
o.hrachov <ol.grachov@gmail.com>
2023-10-13 18:34:08 +0300

    path module has been added

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ require("dotenv").config();
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import path from 'path';
 import { graphqlHTTP } from "express-graphql";
 import { rootSchema }from './schema';
 
@@ -9,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
-app.use(express.static("./src/public"));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 app.use(
