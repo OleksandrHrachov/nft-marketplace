d7abff0fa4366d82dde907052d74352b120636bf
o.hrachov <ol.grachov@gmail.com>
2023-10-17 19:00:31 +0300

    adaptive for footer has been added

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-## NFT MARKETPLACE
+# NFT MARKETPLACE
 
 [DEMO LINK](https://nft-marketplace-client-five.vercel.app/)
+
+## Description 
+
+# Features
+This is a small multi-page site for the sale of NFTs. The site is adapted for tablets and phones, almost all icons, buttons, links and cards are clickable.
+
+# The following technologies were used in the project:
+- NextJS 13
+- TypeScript
+- SASS(SCSS)
+- GraphQL
+- Express
+- MongoDB
+- Mongoose
+
+## RUN PROJECT
+- clone project
+- open a terminal
+- go to the server folder (`cd server`) and run (`npm install`)
+- open new terminal
+- go to the client folder (`cd client`) and run (`npm install`)
+- go back to the server terminal and run (`npm run start`)
+- go back to the client terminal and run (`npm run dev`)
+- follow the link that appears in terminal
---
+++ client/app/components/Footer/SubscribeInput/SubscribeInput.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React, { useState } from "react";
+import { Button } from "../../Button";
+import "./SubscribeInput.scss";
+import mailIcon from "../../../../public/mailIcon.svg";
+
+interface IProps {
+  formClass?: string;
+  buttonClass?: string;
+  inputClass?: string;
+}
+
+let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+
+export default function SubscribeInput({ buttonClass, inputClass, formClass }: IProps) {
+  const [value, setValue] = useState("");
+  
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (regex.test(value)) {
+      setValue("");
+    }
+  };
+
+  return (
+    <form className={`footer-subscribe__input ${formClass}`} onSubmit={onSubmit}>
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className={`footer-subscribe__input-field ${inputClass}`}
+        type="email"
+        required
+        placeholder="Enter your email here"
+      />
+      <Button
+        type="submit"
+        buttonClass={`footer-subscribe__input-button ${buttonClass}`}
+        iconSrc={mailIcon}
+        iconWidth={20}
+        iconHeight={20}
+      >
+        Subscribe
+      </Button>
+    </form>
+  );
+}
